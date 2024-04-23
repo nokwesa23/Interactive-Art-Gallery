9d121ff6b341dea5c52208b1af5820a64816d841
Naiya Okwesa <naiyaokwesa23@gmail.com>
2024-04-23 17:38:11 -0500

    Loaded images and assigned them to a dictonary

--- src/project.py
+++ src/project.py
@@ -2,7 +2,17 @@ import pygame
 
 
 def loading_artworks():
-    return
+    artwork_1 = pygame.image.load('girl-with_a_pearl_earring.jpg')
+    artwork_2 = pygame.image.load('a_sunday_afternoon.jpg')
+    artwork_3 = pygame.image.load('the_kiss.jpg')
+    artwork_4 = pygame.image.load('the_scream.jpg')
+    artwork_5 = pygame.image.load('mona_lisa.jpg')
+    artwork_6 = pygame.image.load('the_starry_night.jpg')
+    
+    artworks = { 'Room1': [(artwork_1), (artwork_2)],
+                 'Room2': [(artwork_3), (artwork_4)],
+                 'Room3': [(artwork_5), (artwork_6)]}
+    return artworks
 
 
 def main():
