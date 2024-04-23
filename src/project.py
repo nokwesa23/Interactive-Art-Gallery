c2477200aa2ad2b1cc3e95d5a4306d49ce42d711
Naiya Okwesa <naiyaokwesa23@gmail.com>
2024-04-23 16:42:35 -0500

    Initialize pygame in main function

---
+++ src/project.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+def main():
+    pygame.int() 
+    pygame.display.set_caption("Interactive Art Gallery")
+    screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+    main()
