7b09d87768b101d028db7b05af1b6654be313788
Naiya Okwesa <naiyaokwesa23@gmail.com>
2024-04-30 15:21:52 -0500

    Readjusted the nested if loop and display artworks feature

--- src/project.py
+++ src/project.py
@@ -35,7 +35,7 @@ def main():
     current_page = 0
     running = True
     while running:
-            for event in pygame.event.get():
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.KEYDOWN:
@@ -43,7 +43,7 @@ def main():
                         current_page = max(current_page - 1, 0)
                     elif event.key == pygame.K_RIGHT:
                         current_page = min(current_page + 1, len(artworks) // artworks_per_page )
-            display_artworks(screen, artworks, current_page * artworks_per_page, current_room)
+        display_artworks(screen, artworks, current_page * artworks_per_page, current_room)
 
     pygame.quit()
 
