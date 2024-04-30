bf625b84cb637bfcfc9db80dd81dd969f7f55d9e
Naiya Okwesa <naiyaokwesa23@gmail.com>
2024-04-30 14:12:21 -0500

    Created load artworks from file function

--- src/project.py
+++ src/project.py
@@ -1,10 +1,13 @@
 import pygame
 
+def load_artworks_from_file():
+    return 
 
 def main():
     pygame.init() 
     pygame.display.set_caption("Interactive Art Gallery")
     screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+    artworks = load
 
     pygame.quit()
 
