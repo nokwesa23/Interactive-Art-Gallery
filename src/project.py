eb85a611139021d6eee804f4da0280febab347c8
Naiya Okwesa <naiyaokwesa23@gmail.com>
2024-04-30 14:23:07 -0500

    Added game loop

--- src/project.py
+++ src/project.py
@@ -9,6 +9,12 @@ def main():
     screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
     artworks = load_artworks_from_file('artworks.txt')
     current_room = 'Room 1'
+    running = True
+    while running:
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
     pygame.quit()
 
