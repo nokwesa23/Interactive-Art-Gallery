165d1b68cf56c5d08643657fe704b6806750378f
Naiya Okwesa <naiyaokwesa23@gmail.com>
2024-04-30 14:37:13 -0500

    Opened file and appended information into a list

--- src/project.py
+++ src/project.py
@@ -1,6 +1,11 @@
 import pygame
 
 def load_artworks_from_file(filename):
+    artworks = []
+    with open(filename, 'r') as file:
+        for line in file:
+            image_path, title, artist, description = line.strip().split(',')
+            artworks.append((image_path, title, artist, description))
     return 
 
 
