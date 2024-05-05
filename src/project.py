9a2f573fc5b55ec31b8060d05c3c18ba1feaae59
Naiya Okwesa <naiyaokwesa23@gmail.com>
2024-05-05 10:54:26 -0500

    For loop to add description to the new surface

--- src/project.py
+++ src/project.py
@@ -37,10 +37,16 @@ def display_artworks(screen, artworks, start_index, artworks_per_page):
     pygame.display.flip()
 
 
-def display_description(screen, descriptioon):
+def display_description(screen, description):
     description_surface = pygame.Surface((screen.get_width() // 2, screen.get_height() // 2))
     description_surface.fill((255,255,255))
-    
+    font = pygame.font.SysFont(None, 20)
+    lines = description.split('\n')
+    y_offset = 20
+    for line in lines:
+        text_surface = font.render(line, True, (0,0,0))
+        description_surface.blit(text_surface, (20, y_offset))
+        y_offset += 30
     
     
     return
